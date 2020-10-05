16b838d93dd88a3c5fd70bda116a4a5466d617c2
mirtauhid <67647821+mirtauhid@users.noreply.github.com>
2020-10-05 17:13:53 +0600

    post method added

--- index.js
+++ index.js
@@ -1,28 +1,43 @@
 const express = require('express')
-require('dotenv').config()
 const MongoClient = require('mongodb').MongoClient;
+const bodyParser = require('body-parser');
+const cors = require('cors');
+require('dotenv').config()
 
 
-const app = express()
 const port = 5000
 
 
+const app = express()
 
-const uri = "mongodb+srv://DB_USER:DB_PASS@cluster0.ey4wq.mongodb.net/DB_NAME?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true });
+
+app.use(cors());
+app.use(bodyParser.json());
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ey4wq.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
-    const collection = client.db("volunteer-network-server").collection("vn.user");
-    console.log("database connected");
-    
+    console.log(err);
+    const registrations = client.db(`${process.env.DB_NAME}`).collection(`${process.env.DB_COL_USER_EVENT}`);
+    console.log("db connected successfully");
+
+
+
+    app.post('/addRegistration', (req, res) => {
+        const newRegistration = req.body;
+        registrations.insertOne(newRegistration)
+            .then(res => {
+                console.log({res});
+            })
+        console.log(newRegistration);
+    })
 });
-
-
 
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
-
-
-app.listen(port);
+app.listen(port)
